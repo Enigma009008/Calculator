fd023263c2286eb098bbfc99bd34c9eed9d88da1
Murad_777 <murad.gadzhiev1999@mail.ru>
2021-11-04 12:23:51 +0300

    v1.0

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public static class Calculator
+    {
+        public static double Addition(double firstNumber, double lastNumber)
+        {
+            return firstNumber + lastNumber;
+        }
+
+        public static double Substraction (double firstNumber, double lastNumber)
+        {
+            return firstNumber - lastNumber;
+        }
+
+        public static double Multiplication(double firstNumber, double lastNumber)
+        {
+            return firstNumber * lastNumber;
+        }
+
+        public static double Division(double firstNumber, double lastNumber)
+        {
+            if ( lastNumber != 0)
+            {
+                return firstNumber / lastNumber;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+}
