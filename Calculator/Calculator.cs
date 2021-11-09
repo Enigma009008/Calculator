93c192c1f803558a17f7bae564d81b968e96e479
Murad_777 <murad.gadzhiev1999@mail.ru>
2021-11-09 19:19:39 +0300

    v2.0

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public static class Calculator
+    {
+        public static double Addition(double firstNumber, double lastNumber)
+        {
+            return firstNumber + lastNumber;
+        }
+
+        public static double Substraction (double firstNumber, double lastNumber)
+        {
+            return firstNumber - lastNumber;
+        }
+
+        public static double Multiplication(double firstNumber, double lastNumber)
+        {
+            return firstNumber * lastNumber;
+        }
+
+        public static double Division(double firstNumber, double lastNumber)
+        {
+            if ( lastNumber != 0)
+            {
+                return firstNumber / lastNumber;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+
+        public static string Discriminant()
+        {
+            Console.Write("Введите число a: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Введите число b: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("Введите число c: ");
+            int c = int.Parse(Console.ReadLine());
+
+            double discriminant = (Power(b, 2) - 4 * a * c);
+            if (discriminant > 0)
+            {
+                double x1 = (-b + Math.Sqrt(discriminant) / (2 * a));
+                double x2 = (-b - Math.Sqrt(discriminant) / (2 * a));
+                return $"x1 = {x1} | x2 = {x2}";
+            }
+            else if (discriminant == 0)
+            {
+                double x = (-b / (2 * a));
+                return $"Дискриминант равен нулю | x = {x}";
+            }
+            else
+            {
+                return "Дискриминант меньше нуля";
+            }
+        }
+
+        public static double Power(double x, int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+
+            if (n % 2 == 0)
+            {
+                var p = Power(x, n / 2);
+                return p * p;
+            }
+            else
+            {
+                return x * Power(x, n - 1);
+            }
+        }
+
+       
+
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        public static double firstNumber { get; set; }
+
+        public static double lastNumber { get; set; }
+
+        public static char action { get; set; }
+        static void Main(string[] args)
+        {
+            while(action != 'e')
+            {             
+                ListCommand();
+                Console.Write("Введите команду: ");
+                action = Convert.ToChar(Console.ReadLine());
+
+                switch (action)
+                {
+                    case '+':
+                        EnterData();
+                        Console.WriteLine("Результат: " + Calculator.Addition(firstNumber, lastNumber));
+                        break;
+                    case '-':
+                        EnterData();
+                        Console.WriteLine("Результат: " + Calculator.Substraction(firstNumber, lastNumber));
+                        break;
+                    case '*':
+                        EnterData();
+                        Console.WriteLine("Результат: " + Calculator.Multiplication(firstNumber, lastNumber));
+                        break;
+                    case '/':
+                        EnterData();
+                        Console.WriteLine("Результат: " + Calculator.Division(firstNumber, lastNumber));
+                        break;
+                    case 'r':                       
+                        Console.Write("Введите число : ");
+                        int value = int.Parse(Console.ReadLine());
+                        Console.Write("Введите степень числа: ");
+                        int n = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Результат: " + Calculator.Power(value, n));
+                        break;
+                    case 'd':                      
+                        Console.WriteLine(Calculator.Discriminant());
+                        break;
+                    case 'e':
+                        Process.GetCurrentProcess().Kill();
+                        break;
+                }
+
+            }            
+        }
+
+      
+        static void EnterData()
+        {
+            Console.Write("Введите первое число: ");
+            firstNumber = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            lastNumber = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public static void ListCommand()
+        {
+
+            Console.WriteLine("+ | Введите для сложения" );
+            Console.WriteLine("- | Введите для вычитания");
+            Console.WriteLine("* | Введите для умножения");
+            Console.WriteLine("/ | Введите для деления"  );
+            Console.WriteLine("r | Введите для возведения числа в степень");
+            Console.WriteLine("d | Введите для нахождения дискриминанта");
+        }
+
+        
+
+    }
+}
